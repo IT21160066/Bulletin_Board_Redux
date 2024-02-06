c6b74cfe0a8abd46d0ee7f51de272c20ba087cac
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-02-06 21:44:18 +0530

    add post form

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -16,9 +16,28 @@ const initialState = [
 const postsSlice = createSlice({
   name: "posts",
   initialState,
-  reducers: {},
+  reducers: {
+    //reducer function to handle the data we submit
+    postAdded(state, action) {
+      state.push(action.payload); //here not mutating the state, immer js useage
+      //here not mutating the state immer js creates new state underneath
+      //this will work only inside createSlice
+    },
+  },
 });
 
 export const selectAllPosts = (state) => state.posts;
 
+export const { postAdded } = postsSlice.actions;
+
 export default postsSlice.reducer;
+
+{
+  /*when we write this postAdded function then create slice automatically generates an action
+  creator function with the same name*/
+}
+
+{
+  /*when we exposrt our actions we actually exporting the action creator function that is 
+  automatically created*/
+}
