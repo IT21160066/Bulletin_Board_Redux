7b7fb21100ab07f05e31ea734556d002de7c6071
Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2024-02-13 23:23:24 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,38 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { fetchUsers } from "./features/users/userSlice";
 
-//we want to load users when app starts
 store.dispatch(fetchUsers());
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
-// import { store } from "./app/store";
-// import { Provider } from "react-redux";
-// import { fetchUsers } from "./features/users/userSlice";
-
-// store.dispatch(fetchUsers());
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
